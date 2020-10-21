308442ac956b04ef88533a087543257859811f47
Jessica Anocibar <jessicayaws@yahoo.com>
2020-10-21 17:14:08 -0600

    pseudo coded and added in files

---
+++ public/assets/js/server.js
@@ -0,0 +1,32 @@
+//add in all paths required
+const express = require ("express");
+const path = require("path");
+const app = express();
+const fs = require ("fs");
+const uuid = require("uuid");
+
+
+
+
+//create a PORT TO use
+
+const PORT = 3000;
+
+
+//create routes
+
+
+
+//display = app.get
+
+
+
+//save = app.post
+
+
+
+//delete = app.delete
+
+
+
+//app.listen
