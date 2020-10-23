148e789ed8c43c6da75f399dde56a949956b4a7b
Jessica Anocibar <jessicayaws@yahoo.com>
2020-10-23 17:00:35 -0600

    modified apiroutes js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,18 +19,21 @@ router.get("/api/notes", (req, res) => {
 
 router.post("/api/notes", function (req, res){
 
-    fs.writeFile(path.join(__dirname, "../db/db.json", "utf8"), (err, data) =>{
+    fs.writeFile(path.join(__dirname, "../db/db.json", "utf8", JSON.stringify(notes)), (err, data) =>{
         if (err) throw err;
         const notes = JSON.parse(data);
         const newNote = req.body;
         notes.push(newNote);
         res.json(newNote);
 
+        res.end();
 
-        //stringify
     });
 
 });
+
+
+
 
 
 });
